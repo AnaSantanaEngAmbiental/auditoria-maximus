b44cbc4ac35a8cd5e839c256fc2373ccd99330b3
AnaSantanaEngAmbiental <anasantana.eng.ambiental@gmail.com>
2026-01-04 03:12:30 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,1 @@
-export default function Teste() {
-  return (
-    <div style={{background: 'black', color: 'yellow', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-      <h1 style={{fontSize: '50px'}}>AGORA MUDOU! VERSAO 37</h1>
-    </div>
-  )
-}
+
